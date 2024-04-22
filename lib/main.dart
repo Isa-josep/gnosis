3f15325230a703536a85307a19ad7a69606f1767
Zeta <zetag174@gmail.com>
2024-04-21 22:45:06 -0600

    Login y register

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:gnosis/config/router/app_router.dart';
 import 'package:flutter/services.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inicio',
-      home: MyHomePage(),
-      
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Inicio de sesión/Registro'),
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    
+    return MaterialApp.router(
+      title: 'Gnosis',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/logo.png'),
-            SizedBox(height: 20),
-            Text('Bienvenido'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/login');
-              },
-              child: Text('Iniciar sesión'),
-            ),
-            SizedBox(height: 10),
-            
-            ElevatedButton(
-              onPressed: () {
-                context.go('/register');
-                },
-              child: Text('Registrarse'),
-            ),
-          ],
-        ),
-      ),
+       routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+     
     );
   }
 }
